461a33f07f7f5a68f239ae95b19c4ac2fcf3dfb2
Bolaji Oyeneye <bioyeneye@gmail.com>
2020-07-09 04:14:00 +0100

    adding specification and query for pagination

--- src/main/java/main/repositories/ProductRepository.java
+++ src/main/java/main/repositories/ProductRepository.java
@@ -1,8 +1,9 @@
 package main.repositories;
 
 import main.entities.Product;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer>, JpaSpecificationExecutor {
     Product findByName(String name);
 }
